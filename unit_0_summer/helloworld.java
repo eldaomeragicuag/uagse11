0384e995d0035c92f01ca154278423d47627aa76
eldaomeragicuag <eldaomeragic@uagateway.org>
2016-10-24 18:09:00 +0000

    initial commit

---
+++ unit_0_summer/helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("I've had an amazing summer exloring my homeland Bosnia!");
+    }
+}
